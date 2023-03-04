a3f17739443970034d9076550e674f899f9f932a
lien <bichlienftu2@gmail.com>
2023-03-04 17:37:40 +0700

    add stock_item_id

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,3 +1,4 @@
 SELECT 
-  order_line_id as order_line_key, quantity, unit_price, quantity*unit_price as gross_amount
+  order_line_id as order_line_key, quantity, unit_price, quantity*unit_price as gross_amount,
+  stock_item_id
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
