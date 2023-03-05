d1e39e6b82c0a7ecebffa60eb66be0a946c4360d
lien <bichlienftu2@gmail.com>
2023-03-05 15:39:28 +0700

    adjust_CTE

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,48 @@
-SELECT 
-  cast (order_line_id as integer) as sales_order_line_key ,cast(stock_item_id as integer) as product_key ,cast(quantity as integer) as quantity, cast(unit_price as numeric) as unit_price, cast(quantity as integer)*cast(unit_price as numeric) as gross_amount ,
-  stock_item_id,cast (customer_id as integer) as customer_key
-FROM `vit-lam-data.wide_world_importers.sales__order_lines` a
-left join `vit-lam-data.wide_world_importers.sales__orders` b on a.order_id =b.order_id 
+with fact_sales_order_line__source as (
+SELECT *  
+FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
+  )
+
+
+, fact_sales_order_line__rename_column as (
+select 
+order_line_id as sales_order_line_key
+,order_id as sales_order_key
+,stock_item_id as product_key
+, quantity
+, unit_price
+from fact_sales_order_line__source
+)
+
+
+, fact_sales_order_line__cast_type as (
+  SELECT
+  cast(sales_order_line_key as integer) as sales_order_line_key
+  ,cast(sales_order_key as integer)as sales_order_key
+  ,cast(product_key as integer) as product_key
+  ,cast(quantity as integer)as quantity
+  ,cast(unit_price as numeric)as unit_price 
+  from fact_sales_order_line__rename_column
+)
+
+
+,fact_sales_order_line__calculate_measure as (
+SELECT
+*,quantity*unit_price as gross_amount
+from fact_sales_order_line__cast_type
+)
+
+select 
+sales_order_line_key
+,sales_order_key
+,product_key
+,quantity
+,unit_price
+,gross_amount
+from fact_sales_order_line__calculate_measure
+
+
+
+
+
+
