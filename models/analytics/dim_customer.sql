d3859f102d5a1bead293b51575bdcc4ab0d7296b
lien <bichlienftu2@gmail.com>
2023-03-05 13:45:29 +0700

    adjust with CTE

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,7 @@
-SELECT 
-  customer_id as customer_key, customer_name
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+with source_dim_customer as (select * from `vit-lam-data.wide_world_importers.sales__customers`
+)
+, rename_source_dim_customer as ( select customer_id as customer_key, customer_name from source_dim_customer)
+
+,cast_source_dim_customer as (select cast(customer_key as integer)as customer_key, cast(customer_name as string)as customer_name from rename_source_dim_customer )
+
+select * from cast_source_dim_customer
