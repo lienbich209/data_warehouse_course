0a4489887309517e302f1958d053922082673919
lien <bichlienftu2@gmail.com>
2023-03-05 15:09:25 +0700

    insert_view_table

--- models/staging/stg_fact_sales_orders.sql
+++ models/staging/stg_fact_sales_orders.sql
@@ -5,6 +5,6 @@ with fact_sales_order_source as
 (select order_id as order_key, customer_id as customer_key from fact_sales_order_source)
 
 , fact_sales_order__cast_type as
-(select cast(order_key as integer) as order_key,cast(customer_key as integer) as customer_key from fact_sales_order_rename)
+(select cast(order_key as integer) as order_key,cast(customer_key as integer) as customer_key from fact_sales_order__rename_column)
 
-select order_key,customer_key from fact_sales_order_cast
+select order_key,customer_key from fact_sales_order__cast_type
