8fa16930f86de10df2741e9704eb0dc5fea05c13
lien <bichlienftu2@gmail.com>
2023-03-09 21:22:05 +0700

    flatten1

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,13 +1,36 @@
 with 
 
-dim_product_rename_column as(
+dim_product__source as(
 SELECT 
-  stock_item_id as product_key, stock_item_name as product_name, brand as brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`),
+      * 
+FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
 
-source_dim_product
-as (SELECT 
-cast(product_key as integer) as product_key, cast(product_name as string) as product_name, cast(brand_name as string) as brand_name
-FROM dim_product_rename_column)
+,dim_product__rename_column as (
+  SELECT 
+    stock_item_id as product_key
+    ,stock_item_name as product_name
+    ,brand as brand_name
+    ,supplier_id as supplier_key
+  from
+    dim_product__source
+)
 
-select * from source_dim_product
+,dim_product__cast_type as (
+  SELECT 
+    cast(product_key as integer) as product_key
+    ,cast(product_name as string) as product_name
+    ,cast(brand_name as string) as brand_name
+    ,cast(supplier_key as integer) as supplier_key
+FROM dim_product__rename_column
+)
+
+select 
+  dim_product.product_key
+  ,dim_product.product_name
+  ,dim_product.brand_name
+  ,dim_product.supplier_key
+  ,dim_supplier.supplier_name
+from dim_product__cast_type as dim_product
+left join {{ref(dim_supplier)}} as dim_supplier
+on dim_product.supplier_key=dim_supplier.supplier_key
