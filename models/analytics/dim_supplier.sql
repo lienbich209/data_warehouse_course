dce24d59b7fab2e1d1ab6bf95841ee9e9075f22d
lien <bichlienftu2@gmail.com>
2023-03-09 20:48:27 +0700

    create_dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,26 @@
-SELECT 
+
+with dim_supplier__source as (
+
+  SELECT 
   *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_column as (
+  SELECT 
+    supplier_id as supplier_key
+    ,supplier_name as supplier_name 
+  from dim_supplier__source
+)
+
+, dim_supplier__cast_type as (
+  SELECT
+    cast(supplier_key as integer) as supplier_key
+    ,cast(supplier_name as string) as supplier_name
+  from dim_supplier__rename_column
+)
+
+SELECT
+  supplier_key
+  ,supplier_name
+from dim_supplier__cast_type
