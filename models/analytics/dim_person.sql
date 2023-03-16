82b7764875615690bbb0d832514bbee22bc94ba4
lien <bichlienftu2@gmail.com>
2023-03-16 20:50:12 +0700

    dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,27 @@
-SELECT 
+
+with dim_person__source as (
+
+  SELECT 
   *
-FROM `vit-lam-data.wide_world_importers.application__people`
+  FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename_column as (
+  SELECT 
+    person_id as person_key
+    ,full_name as full_name 
+  from dim_person__source
+)
+
+, dim_person__cast_type as (
+  SELECT
+    cast(person_key as integer) as person_key
+    ,cast(full_name as string) as full_name
+  from dim_person__rename_column
+)
+
+SELECT
+  person_key
+  ,full_name
+from dim_person__cast_type
+
