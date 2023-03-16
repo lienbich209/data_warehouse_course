e319b749441ce08d3ca591b80f2f8b332bb79335
lien <bichlienftu2@gmail.com>
2023-03-16 20:54:49 +0700

    add_picked_by_person_key

--- models/staging/stg_fact_sales_orders.sql
+++ models/staging/stg_fact_sales_orders.sql
@@ -1,10 +1,25 @@
 with fact_sales_order_source as
 (SELECT * FROM `vit-lam-data.wide_world_importers.sales__orders` )
 
-, fact_sales_order__rename_column as 
-(select order_id as order_key, customer_id as customer_key from fact_sales_order_source)
+, fact_sales_order__rename_column as (
+select 
+  order_id as order_key
+  , customer_id as customer_key
+  ,picked_by_person_id as picked_by_person_key
+from 
+  fact_sales_order_source)
 
-, fact_sales_order__cast_type as
-(select cast(order_key as integer) as order_key,cast(customer_key as integer) as customer_key from fact_sales_order__rename_column)
+, fact_sales_order__cast_type as(
+select 
+  cast(order_key as integer) as order_key
+  ,cast(customer_key as integer) as customer_key
+  ,cast(picked_by_person_key as integer) as picked_by_person_key
+from 
+fact_sales_order__rename_column)
 
-select order_key,customer_key from fact_sales_order__cast_type
+select 
+  order_key
+  ,customer_key 
+  ,picked_by_person_key
+from 
+  fact_sales_order__cast_type
