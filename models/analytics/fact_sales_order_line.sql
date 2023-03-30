88ade6fed609544bd89ba33531fd53a0ae80fe48
lien <bichlienftu2@gmail.com>
2023-03-30 19:16:21 +0700

    add

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -9,8 +9,12 @@ select
 order_line_id as sales_order_line_key
 ,order_id as sales_order_key
 ,stock_item_id as product_key
-, quantity
-,unit_price,
+,package_type_id as package_type_key
+,quantity
+,unit_price
+,tax_rate
+,description
+,picked_quantity
 from fact_sales_order_line__source
 )
 
@@ -20,9 +24,13 @@ from fact_sales_order_line__source
   cast(sales_order_line_key as integer) as sales_order_line_key
   ,cast(sales_order_key as integer)as sales_order_key
   ,cast(product_key as integer) as product_key
+  ,cast(package_type_key as integer) as package_type_key
   ,cast(quantity as integer)as quantity
   ,cast(unit_price as numeric)as unit_price 
-  from fact_sales_order_line__rename_column
+  ,cast(tax_rate as numeric) as tax_rate
+  ,cast(description as string) as description
+  ,cast(picked_quantity as integer) as picked_quantity
+from fact_sales_order_line__rename_column
 )
 
 
@@ -42,15 +50,22 @@ fact_line.sales_order_line_key
 ,fact_line.quantity
 ,fact_line.unit_price
 ,fact_line.gross_amount
+,fact_line.tax_rate
+,fact_line.description
+,fact_line.picked_quantity
 ,fact_header.customer_key
 ,fact_header.order_date
 ,coalesce (fact_header.picked_by_person_key,-1) as picked_by_person_key
 ,fact_person.full_name
+,fact_line.package_type_key
+,fact_package.package_type_name
 from fact_sales_order_line__calculate_measure as fact_line
 left join {{ref('stg_fact_sales_orders')}} as fact_header 
 on fact_line.sales_order_key=fact_header.order_key
 left join {{ref('dim_person')}} as fact_person
 on fact_header.picked_by_person_key=fact_person.person_key
+left join {{ref('dim_package_type')}} as fact_package
+on fact_line.package_type_key=fact_package.package_type_key
 
 
 /*select 
